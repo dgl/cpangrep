dgl/cpangrep

commit 1ac66e32fd9e1d09b00ff4b186e4bc65499c64b7

Show time even for no results

lib/WWW/CPANGrep.pm
@@ -106,7 +106,8 @@ sub render_response {
 
   if($error || !@$results) {
     return $output
-      ->select('.divider')->replace_content(" ")
+      ->select('#result-count')->replace_content(" ")
+      ->select('#time')->replace_content(sprintf "%0.2f", $duration)
       ->select('.result')->replace_content($error || "No matches found.")
       ->select('.pagination')->replace("");
   }

share/html/results.html
@@ -37,7 +37,7 @@
 	</div>
 
         <div class="divider">
-           <strong><span id="start-at">1</span> to <span id="end-at">20</span></strong> of <strong><span id="total">about 1,000,000</span> results</strong> (<span id="time">4.2</span> seconds)
+           <span id="result-count"><strong><span id="start-at">1</span> to <span id="end-at">20</span></strong> of <strong><span id="total">about 1,000,000</span> results</strong></span> (<span id="time">4.2</span> seconds)
 	</div>
 
 	<div class="results">

commit 3a184e4cfc5c1515c4edd7f3067cd02fbd08ce12

Show actual errors rather than Internal Server Errors
(Broken by 1ac66e3)

lib/WWW/CPANGrep.pm
@@ -99,9 +99,15 @@ sub render_response {
     ->select('input[name="q"]')->add_to_attribute(value => $q);
 
   if($error || !@$results) {
+    if($error) {
+      $output = $output->select('.divider')->replace_content("Error");
+    } else {
+      $output = $output->select('#result-count')->replace_content(" ")
+        ->select('#time')->replace_content(
+          $duration ? sprintf "%0.2f", $duration: "")
+    }
+
     return $output
-      ->select('#result-count')->replace_content(" ")
-      ->select('#time')->replace_content(sprintf "%0.2f", $duration)
       ->select('.result')->replace_content($error || "No matches found.")
       ->select('.pagination')->replace("");
   }